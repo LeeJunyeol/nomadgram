2e505ec51488e1c9d2284e16f9b8773a81a44bea
LeeJunyeol <chljy33@naver.com>
2018-01-02 21:42:48 +0900

    customizing django admin

--- nomadgram/images/admin.py
+++ nomadgram/images/admin.py
@@ -4,12 +4,45 @@ from . import models
 @admin.register(models.Image)
 class ImageAdmin(admin.ModelAdmin):
   # pass: 이 클래스는 텅빈 클래스다.
-  pass 
+  list_display_links = (
+    'location',
+    'caption'
+  )
+
+  search_fields = (
+    'location',
+    'caption'
+  )
+
+  list_filter = (
+    'location',
+    'creator'
+  )
+
+  list_display = (
+    'file',
+    'location',
+    'caption',
+    'creator',
+    'created_at',
+    'updated_at',
+  )
 
 @admin.register(models.Like)
 class LikeAdmin(admin.ModelAdmin):
-  pass
+  list_display = (
+      'creator',
+      'image',
+      'created_at',
+      'updated_at',
+  )
 
 @admin.register(models.Comment)
 class CommentAdmin(admin.ModelAdmin):
-  pass
+  list_display = (
+      'message',
+      'creator',
+      'image',
+      'created_at',
+      'updated_at'
+  )
