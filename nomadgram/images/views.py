dde163b1b12a5c7c8e59ee578fa60a2f8e63368e
LeeJunyeol <chljy33@naver.com>
2018-01-14 17:50:13 +0900

    #1-39 Getting the User Feed part One

--- nomadgram/images/views.py
+++ nomadgram/images/views.py
@@ -5,4 +5,19 @@ from . import models, serializers
 
 class Feed(APIView):
   def get(self, request, format=None):
-    pass  
+    user = request.user
+    following_users = user.followings.all()
+    image_list = []
+
+    for following_user in following_users:
+      user_images = following_user.images.all()[:2]
+      for image in user_images:
+        image_list.append(image)  
+
+    print(image_list)
+    sorted_list = sorted(image_list, key=lambda image: image.created_at, reverse=True)
+    serializer = serializers.ImageSerializer(sorted_list, many=True)
+    return Response(serializer.data)
+
+# def get_key(image):
+#   return image.created_at
