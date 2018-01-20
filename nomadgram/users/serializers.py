3fa1593d98ec7cb1f1d11fe598b12f0d1759d63e
LeeJunyeol <chljy33@naver.com>
2018-01-20 20:03:00 +0900

    #1-51 Getting User Profile

--- nomadgram/users/serializers.py
+++ nomadgram/users/serializers.py
@@ -1,5 +1,24 @@
 from rest_framework import serializers
 from . import models
+from nomadgram.images import serializers as image_serializers
+
+class UserProfileSerializer(serializers.ModelSerializer):
+  
+  images = image_serializers.UserProfileImageSerializer(many=True)
+  
+  class Meta:
+    model = models.User
+    fields = (
+      'id',
+      'username',
+      'name',
+      'bio',
+      'website',
+      'post_count',
+      'followers_count',
+      'followings_count',
+      'images'
+    )
 
 class ExploreUserSerializer(serializers.ModelSerializer):
 
