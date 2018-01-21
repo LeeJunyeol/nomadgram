842b948257616b9c87ef7626959439af9c5bd951
LeeJunyeol <chljy33@naver.com>
2018-01-21 18:26:55 +0900

    #1-62 Including Myself on the Feed

--- nomadgram/images/views.py
+++ nomadgram/images/views.py
@@ -11,10 +11,17 @@ class Feed(APIView):
     following_users = user.followings.all()
     image_list = []
 
+    # 내가 팔로잉한 유저의 이미지 
     for following_user in following_users:
       user_images = following_user.images.all()[:2]
       for image in user_images:
         image_list.append(image)  
+
+    # 내 이미지도 피드에 추가 
+    my_images = user.images.all()[:2]
+
+    for image in my_images:
+      image_list.append(image)
 
     sorted_list = sorted(image_list, key=lambda image: image.created_at, reverse=True)
     serializer = serializers.ImageSerializer(sorted_list, many=True)
