b454db60c07b03bb2bd2bbf55acaf116d6e5ea23
LeeJunyeol <chljy33@naver.com>
2018-01-21 18:06:28 +0900

    #1-60 Creating Follow, Comment and Like notification

--- nomadgram/images/views.py
+++ nomadgram/images/views.py
@@ -3,6 +3,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from . import models, serializers
+from nomadgram.notifications import views as notification_views
 
 class Feed(APIView):
   def get(self, request, format=None):
@@ -40,6 +41,8 @@ class LikeImage(APIView):
       )
 
       new_like.save()
+
+      notification_views.create_notification(user, found_image.creator, 'like', found_image)
 
       return Response(status=status.HTTP_201_CREATED)
 
@@ -80,6 +83,9 @@ class CommentOnImage(APIView):
 
     if serializer.is_valid():
       serializer.save(creator=user, image=found_image)
+
+      notification_views.create_notification(user, found_image.creator, 'comment', found_image, serializer.data['message'])
+
       return Response(data=serializer.data, status=status.HTTP_200_OK)
 
     else:
