bf7030bf473ea2be2363cc81561578e87bd23d82
LeeJunyeol <chljy33@naver.com>
2018-01-23 21:53:38 +0900

    #1-67 Update Profile

--- nomadgram/users/serializers.py
+++ nomadgram/users/serializers.py
@@ -5,6 +5,9 @@ from nomadgram.images import serializers as image_serializers
 class UserProfileSerializer(serializers.ModelSerializer):
   
   images = image_serializers.CountImageSerializer(many=True)
+  post_count = serializers.ReadOnlyField()
+  followers_count = serializers.ReadOnlyField()
+  followings_count = serializers.ReadOnlyField()
   
   class Meta:
     model = models.User
@@ -17,7 +20,8 @@ class UserProfileSerializer(serializers.ModelSerializer):
       'post_count',
       'followers_count',
       'followings_count',
-      'images'
+      'images',
+      'profile_image'
     )
 
 class ListUserSerializer(serializers.ModelSerializer):
