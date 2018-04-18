f283e2c5a748a56583b162d52ac66edadf5a29f4
LeeJunyeol <chljy33@naver.com>
2018-04-18 21:56:00 +0900

    #3-32 Importing Translations

--- frontend/src/translations.js
+++ frontend/src/translations.js
@@ -1,6 +1,40 @@
 export const translations = {
-  es: {
-    "Log in": "Iniciar Session",
-    "About Us": "Sobre nosotros"
-  }
+  'es': {
+    'Have an account?': 'Tienes una cuenta?',
+    'Log in': 'Iniciar Sesión',
+    'Don\'t have an account?': 'No tienes una cuenta?',
+    'Sign up': '',
+    'Get the app': '',
+    'About Us': '',
+    'Support': '',
+    'Blog': '',
+    'Press': '',
+    'API': '',
+    'Jobs': '',
+    'Privacy': '',
+    'Terms': '',
+    'Directory': '',
+    'Language': '',
+  },
+  'options': {
+    'plural_rule': 'n != 1',
+    'plural_number': '2',
+  },
+  'ko': {
+    'Have an account?': '계정이 있으신가요?',
+    'Log in': '로그인',
+    'Don\'t have an account?': '계정이 없으신가요?',
+    'Sign up': '회원가입',
+    'Get the app': '앱을 다운로드하세요',
+    'About Us': '인스타그램 정보',
+    'Support': '지원',
+    'Blog': '블로그',
+    'Press': '홍보 센터',
+    'API': '',
+    'Jobs': '채용 정보',
+    'Privacy': '개인정보처리방침',
+    'Terms': '약관',
+    'Directory': '디렉터리',
+    'Language': '언어',
+  },
 }
