655be2fcd42308031fe1cb2f4685fb16a0e4a1d7
LeeJunyeol <chljy33@naver.com>
2018-04-18 22:15:45 +0900

    #3-33 Moving the AuthForm to their own components

--- frontend/src/components/Auth/presenter.js
+++ frontend/src/components/Auth/presenter.js
@@ -1,6 +1,7 @@
 import React from "react";
 import styles from "./styles.scss";
-import { LoginForm, SignupForm } from "components/AuthForms";
+import SignupForm from "components/SignupForm";
+import LoginForm from "components/LoginForm";
 import PropTypes from "prop-types";
 
 const Auth = (props, context) => (
--- frontend/src/components/SignupForm/presenter.js
+++ frontend/src/components/SignupForm/presenter.js
@@ -1,32 +1,9 @@
 import React from "react";
 import PropTypes from "prop-types";
 import Ionicon from "react-ionicons";
-import styles from "./styles.scss";
+import styles from "shared/formStyles.scss";
 
-export const LoginForm = (props, context) => (
-  <div className={styles.formComponent}>
-    <form className={styles.form}>
-      <input type="text" placeholder="Username" className={styles.textInput} />
-      <input
-        type="password"
-        placeholder="Password"
-        className={styles.textInput}
-      />
-      <input type="submit" value="Log in" className={styles.button} />
-    </form>
-    <span className={styles.divider}>or</span>
-    <span className={styles.facebookLink}>
-      <Ionicon icon="logo-facebook" fontSize="20px" color="#385185" /> {context.t("Log in with Facebook")}
-    </span>
-    <span className={styles.forgotLink}>{context.t("Forgot password?")}</span>
-  </div>
-);
-
-LoginForm.contextTypes = {
-  t: PropTypes.func.isRequired
-}
-
-export const SignupForm = (props, context) => (
+const SignupForm = (props, context) => (
   <div className={styles.formComponent}>
     <h3 className={styles.signupHeader}>
       {context.t("Sign up to see photos and videos from your friends.")}
@@ -59,3 +36,5 @@ export const SignupForm = (props, context) => (
 SignupForm.contextTypes = {
   t: PropTypes.func.isRequired
 }
+
+export default SignupForm;
